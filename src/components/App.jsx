26f702dfd06347dc3129a064b6a5a1ee6d523496
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2022-05-24 22:45:27 -0500

    empty project, add the intial settings for the project

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/global.scss";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello FUCKING World !!!</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("app"));
+
+//******************* APUNTES ********************
+//@babel/core Instala el core de babel
+//@babel/preset - env Instala babel para trabrjar con JS MODERNO EnmaScript 6
+//@babel/preset - react  Instala babel para trabajar con todas las bondaddes REACT
