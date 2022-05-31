b38b71b3b783bdc62380b315a89e5e4a46526c8f
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2022-05-30 22:03:39 -0500

    add the components, containers pages and styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import ProductList from "../containers/ProductList";
 import Header from "../components/Header";
 
 const Home = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <ProductList />
+    </>
+  );
 };
 
 export default Home;
