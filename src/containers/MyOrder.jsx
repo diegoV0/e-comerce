377bbcf362a2d41153764efaf1195c94680182dc
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2022-06-03 23:12:28 -0500

    add the functions  for calculate the total of the order and for remove any item of the cart

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -1,11 +1,18 @@
-import React, { useContext } from "react"; //tarbjar el 404 not found, poner en el nimero de items en el carrito 9+, mostar mi orden solo si hay mas d eun item
-import AppContext from "../context/AppContext";
+import React, { useContext } from "react"; //tarbjar el 404 not found, poner en el nimero de items en el carrito 9+, mostar mi orden solo si hay mas d eun item,
+import AppContext from "../context/AppContext"; //cuando se agreag mas de un mismo item, borrar todos al darle X
 import OrderItem from "@components/OrderItem";
 import "@styles/MyOrder.scss";
 import arrow from "@icons/flechita.svg";
 
 const MyOrder = () => {
   const { state } = useContext(AppContext);
+
+  const sumTotal = () => {
+    const reducer = (accumulator, currentValue) =>
+      accumulator + currentValue.price;
+    const sum = state.cart.reduce(reducer, 0);
+    return sum;
+  };
 
   return (
     <aside className="MyOrder">
@@ -22,7 +29,7 @@ const MyOrder = () => {
           <p>
             <span>Total</span>
           </p>
-          <p>$560.00</p>
+          <p>${sumTotal()}</p>
         </div>
         <button className="primary-button">Checkout</button>
       </div>
