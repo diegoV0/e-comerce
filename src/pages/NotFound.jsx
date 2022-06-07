c9a93c9b6020f38b2869de426178cfa91f8aaf18
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2022-06-06 22:32:59 -0500

    add styles for NotFound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "@styles/NotFound.scss";
 
-const NotFound = () => {
+export const NotFound = () => {
   return (
-    <div>
-      <h1>ERROR 404</h1>
-      <p>Página no encontrada</p>
+    <div className="NotFound">
+      <img
+        className="NotFoundImage"
+        src="https://i.pinimg.com/originals/86/41/80/86418032b715698a4dfa6684b50c12af.gif"
+        alt="Not Found"
+      />
+      <Link className="LinkToHome" to="/">
+        <p className="NotFoundText">Return to Home</p>
+      </Link>
     </div>
   );
 };
